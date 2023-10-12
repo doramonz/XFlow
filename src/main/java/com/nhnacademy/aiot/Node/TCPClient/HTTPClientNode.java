4d2d1da71eeb2dde441415be291b69eeb1223c2e
HyeSeung <32805709+doramonz@users.noreply.github.com>
2023-10-12 17:45:15 +0900

    노드

--- src/main/java/com/nhnacademy/aiot/Node/TCPClient/HTTPClientNode.java
+++ src/main/java/com/nhnacademy/aiot/Node/TCPClient/HTTPClientNode.java
@@ -1,5 +1,23 @@
 package com.nhnacademy.aiot.Node.TCPClient;
 
-public class HTTPClientNode {
+public class HTTPClientNode extends ActiveNode {
+
+    @Override
+    public void preProcess() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'preProcess'");
+    }
+
+    @Override
+    public void postProcess() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'postProcess'");
+    }
+
+    @Override
+    public void process() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'process'");
+    }
     
 }
